cb538e1537362f05ea51b39dcee0658595895999
abdo <abdelrahmanhatem678@yahoo.com>
2026-08-30 22:12:24 +0300

    login_fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,30 @@
 // TODO: Handle login requests
 import jwt from "jsonwebtoken"
-
+import User from "../models/User.js"
+import bcrypt from "bcrypt"
 export async function loginController(req, res, next) {
   try {
     const {email ,password} = req.body
     if(!email || !password){
       return res.status(400).json({Error:"email and password must be provided"})
     }
-    const token =jwt.sign({
-      role:"admin"
-    },process.env.JWT_SECRET
-    ,{expiresIn:"1d"})
-    return res.status(200).json({token})
+    const admin = await User.findOne({email:email})
+    if(admin){
+        const check  = await bcrypt.compare(password, admin.passwordHash)
+        if(admin && check){
+          const token =jwt.sign({
+          role:"admin"
+          },process.env.JWT_SECRET
+          ,{expiresIn:"1d"})
+          return res.status(200).json({token})
+        }else{
+          return res.status(400).json({error:"invaild username or password"})
+        }
+    }else{
+          return res.status(400).json({error:"invaild username or password"})
+        }
+    
+    
     // Get email and password from request body
     // Check if both fields are provided
     // Try to login with provided credentials
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { loginController } from "../controllers/authController.js";
-
+import { rateLimit } from 'express-rate-limit'
 // Create router for authentication routes
 const router = express.Router();
-
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 5, 
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+})
 // POST /api/v1/auth/login - Login endpoint
-router.post("/api/v1/auth/login", loginController);
+router.post("/api/v1/auth/login",limiter, loginController);
 
 export default router;
